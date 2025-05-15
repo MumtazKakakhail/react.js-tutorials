ba6a9bcb32ea642e0aef11eba2d4d11b459efd18
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-05-15 22:41:20 +0500

    react.js tuts

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 //components in jsx
 export const App = () => {
   return <div>
-<NetflixSeries/>
-<NetflixSeries/>
-<NetflixSeries/>
-<NetflixSeries/>
-<NetflixSeries/>
+    <NetflixSeries />
+    <NetflixSeries />
+    <NetflixSeries />
+    <NetflixSeries />
+    <NetflixSeries />
   </div>
 };
 
 const NetflixSeries = () => {
-  return  <div>
-      <div><img src="../public/harry.png" alt="" width="40%" height="40%" /></div>
-      <h2>Name: Harry Potter and the Deathly Hallows – Part 2 (2011)</h2>
-      <h3>Rating: 96%</h3>
-      <p>Summary: A young wizard, Harry Potter, battles dark forces at Hogwarts, uncovering secrets,
-        forging friendships, and confronting Voldemort to save magic</p>
-    </div>
+  return <div>
+    <div><img src="../public/harry.png" alt="" width="40%" height="40%" /></div>
+    <h2>Name: Harry Potter and the Deathly Hallows – Part 2 (2011)</h2>
+    <h3>Rating: 96%</h3>
+    <p>Summary: A young wizard, Harry Potter, battles dark forces at Hogwarts, uncovering secrets,
+      forging friendships, and confronting Voldemort to save magic</p>
+  </div>
 }
