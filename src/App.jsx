f5cf3ced068720569b75abdffcf2e4adf6b407c4
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-05-15 20:17:11 +0500

    React.js Tutorials

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,43 @@
 
 export const App = () => {
   return <div>
-    <div><img src="../public/harry.png" alt="" width="40%" height="40%" /></div>
-    <h2>Name: Harry Potter and the Deathly Hallows – Part 2 (2011)</h2>
-    <h3>Rating: 96%</h3>
-    <p>A young wizard, Harry Potter, battles dark forces at Hogwarts, uncovering secrets,
-       forging friendships, and confronting Voldemort to save magic</p>
+    <div>
+      <div><img src="../public/harry.png" alt="" width="40%" height="40%" /></div>
+      <h2>Name: Harry Potter and the Deathly Hallows – Part 2 (2011)</h2>
+      <h3>Rating: 96%</h3>
+      <p>Summary: A young wizard, Harry Potter, battles dark forces at Hogwarts, uncovering secrets,
+        forging friendships, and confronting Voldemort to save magic</p>
+    </div>
+    <div>
+      <div>
+        <img src="" alt="" />
+      </div>
+      <h2>Name: Supacell</h2>
+      <h3>Rating: 100%</h3>
+      <p>Summary: Five Black South Londoners gain superpowers and unite to protect loved ones from
+         a secretive organization.</p>
+    </div>
+    <div>
+      <div><img src="" alt="" /></div>
+      <h2>Name: Baby Reindeer</h2>
+      <h3>Rating: 99%</h3>
+      <p>Summary: A comedian's life unravels as he navigates the psychological aftermath of being 
+        stalked by a pub patron.</p>
+    </div>
+    <div>
+      <div><img src="" alt="" /></div>
+      <h2>Name: American Nightmare</h2>
+      <h3>Rating: 96%</h3>
+      <p>Summary: A gripping true-crime docuseries examining the 2015 kidnapping of Denise Huskins and 
+        the ensuing media frenzy.</p>
+    </div>
+    <div>
+      <div><img src="" alt="" /></div>
+      <h2>Name: One Day</h2>
+      <h3>Rating: 91%</h3>
+      <p>Summary: Chronicles a 20-year relationship between two opposites, meeting annually to explore 
+        love, loss, and growth.</p>
+    </div>
   </div>
-
 };
 
