c2584f896d12633befac891eadc98282eba52a6a
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-05-16 19:06:23 +0500

    Dynamic values in jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
 import React from "react";
 //components in jsx
 export const App = () => {
-  return ( 
+  return (
     <>
-    <NetflixSeries />
-    <NetflixSeries />
-    <NetflixSeries />
-    <NetflixSeries />
-    <NetflixSeries />
-  </>
+      <NetflixSeries />
+      <NetflixSeries />
+      <NetflixSeries />
+      <NetflixSeries />
+      <NetflixSeries />
+    </>
   );
 };
 
 const NetflixSeries = () => {
+  //Dynamic values in jsx
+  const name = "Harry Potter and the Deathly Hallows – Part 2 (2011)";
+  const rating = "96%";
+  const summary =
+  "A young wizard, Harry Potter, battles dark forces at Hogwarts, uncovering secrets, forging friendships, and confronting Voldemort to save magic";
+
   return <div>
     <div><img src="../public/harry.png" alt="" width="40%" height="40%" /></div>
-    <h2>Name: Harry Potter and the Deathly Hallows – Part 2 (2011)</h2>
-    <h3>Rating: 96%</h3>
-    <p>Summary: A young wizard, Harry Potter, battles dark forces at Hogwarts, uncovering secrets,
-      forging friendships, and confronting Voldemort to save magic</p>
+    <h2>Name: {name} </h2>
+    <h3>Rating: {rating} </h3>
+    <p>Summary: {summary} </p>
   </div>
 }
