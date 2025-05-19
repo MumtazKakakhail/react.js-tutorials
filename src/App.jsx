d838b870107d66eb3f5d57b98749765351cb9740
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-05-19 20:27:27 +0500

    Conditional rendering in jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { CardComponents, NetflixSeries } from './Components/CardComponents'
+import ConditionalRendering from './Components/ConditionalRendering'
 
 function App() {
   return (
     <div>
       <CardComponents />
       <NetflixSeries />
+      <ConditionalRendering/>
     </div>
   )
 }
