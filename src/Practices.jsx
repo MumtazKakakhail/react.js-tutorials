904cb0d06a13ed64ff9d03bb8a0e017f8078d4e3
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-05-20 19:33:40 +0500

    Interview questios

--- src/Practices.jsx
+++ src/Practices.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 export const Practices = () => {
-    const students = [];
+    const students = [1];
     return (
     <>
-    <p>{students.length && "No students found"}</p>
+    {/* <p>{students.length && "No students found"}</p> */}
     <p>Number of students : {students.length}</p>
+    <p>{students.length === 0 && "No students found" }</p>
     </>
-    )
-}
+    );
+};
 export default Practices
