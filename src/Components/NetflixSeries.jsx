603ea59b01142eca978deb68ad7b7d4312c68287
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-05-21 00:17:01 +0500

    Netflix cards using react.js

--- src/Components/NetflixSeries.jsx
+++ src/Components/NetflixSeries.jsx
@@ -2,24 +2,24 @@ import seriesData from "../api/seriesData.json";
 const NetflixSeries = () => {
     return (
 
-<ul>
-    {seriesData.map((curElem) => {
-        return(
- <li key={curElem.id}>
-      <div>
-        <img src= {curElem.img_url} alt="curElem.name" width="40%" height="40%" />
-        </div>
-    <h2>Name: {curElem.name} </h2>
-    <h3>Rating: {curElem.rating} </h3>
-     <p>Summary: {curElem.summary} </p>
-    <p>Genre: {curElem.genre}</p>
-    <a href={curElem.watch_url} target= "_blank">
-        <button>Watch Now</button>
-    </a>
-</li>)
-    })}
-   
-    </ul>
+        <ul>
+            {seriesData.map((curElem) => {
+                return (
+                    <li key={curElem.id}>
+                        <div>
+                            <img src={curElem.img_url} alt="curElem.name" width="40%" height="40%" />
+                        </div>
+                        <h2>Name: {curElem.name} </h2>
+                        <h3>Rating: {curElem.rating} </h3>
+                        <p>Summary: {curElem.summary} </p>
+                        <p>Genre: {curElem.genre}</p>
+                        <a href={curElem.watch_url} target="_blank">
+                            <button>Watch Now</button>
+                        </a>
+                    </li>)
+            })}
+
+        </ul>
 
     );
 }
