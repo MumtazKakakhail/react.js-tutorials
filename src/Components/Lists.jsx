2aaf418cf5b4679a6529a27fe6df49ae1fa37373
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-05-21 21:24:39 +0500

    Props in react.js

--- src/Components/Lists.jsx
+++ src/Components/Lists.jsx
@@ -1,5 +1,5 @@
 export const SeriesCard = (props) => {
-    const { id, img_url, name, rating, summary, genre, watch_url } = props.data;
+    const {  img_url, name, rating, summary, genre, cast, watch_url } = props.data;
     return (
         <li key={props}>
             <div>
@@ -8,7 +8,8 @@ export const SeriesCard = (props) => {
             <h2>Name: {name} </h2>
             <h3>Rating: {rating} </h3>
             <p>Summary: {summary} </p>
-            <p>Genre: {genre}</p>
+            <p>Genre: {genre} </p>
+            <p>Cast: {cast} </p>
             <a href={watch_url} target="_blank">
                 <button>Watch Now</button>
             </a>
