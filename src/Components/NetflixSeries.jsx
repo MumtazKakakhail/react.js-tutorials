826a4c7b6f4d92cf38e2b29177a96dc91823abe1
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-05-21 20:04:26 +0500

    Props in react.js

--- src/Components/NetflixSeries.jsx
+++ src/Components/NetflixSeries.jsx
@@ -2,11 +2,11 @@ import seriesData from "../api/seriesData.json";
 import { SeriesCard } from "./Lists";
 const NetflixSeries = () => {
     return (
-    <ul>
-        {seriesData.map((curElem) => (
-        <SeriesCard key={curElem.id} curElem={curElem} />
-    ))}
-    </ul>
+        <ul>
+            {seriesData.map((curElem) => (
+                <SeriesCard key={curElem.id} curElem={curElem} />
+            ))}
+        </ul>
     );
 };
 export default NetflixSeries;
