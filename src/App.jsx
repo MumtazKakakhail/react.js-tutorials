a3c9839cf1d9414611d04d3eae5d11a997f27fec
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-05-22 23:59:10 +0500

    adding css to react.js

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,29 @@ import React from 'react'
 // import { CardComponents } from './Components/CardComponents'
 // import ConditionalRendering from './Components/ConditionalRendering'
 // import Practices from './Practices'
-import NetflixSeries from './Components/NetflixSeries'
-import Profile from './Components/Profile'
-
-function App() {
+import NetflixSeries from './Components/NetflixSeries';
+import Profile from './Components/Profile';
+import "./Components/Netflix.css"
+export const App = () => {
   return (
-    <div>
-      {/* <CardComponents /> */}
-      <NetflixSeries/>
-      {/* <ConditionalRendering/> */}
-      {/* <Practices/> */}
-      {/* <Profile/> */}
-    </div>
-  )
-}
+    <section className="container">
+      <h1 className="card-heading">List of Best Netflix Series</h1>
+      <NetflixSeries />
+    
+    </section>
+  );
+};
 
-export default App
+export default App;
+
+// function App() {
+//   return (
+//     <div>
+//       <CardComponents />
+//       <NetflixSeries/>
+//       <ConditionalRendering/>
+//       <Practices/>
+//       <Profile/>
+//     </div>
+//   )
+// }
