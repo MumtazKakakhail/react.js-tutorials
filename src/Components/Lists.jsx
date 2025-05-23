b2b4b18fbbe6e587c0f972fdd9431d244b0528f1
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-05-23 18:45:19 +0500

    Adding css to react.js

--- src/Components/Lists.jsx
+++ src/Components/Lists.jsx
@@ -1,10 +1,11 @@
 export const SeriesCard = ({data}) => {
     const {  img_url, name, rating, summary, genre, cast, watch_url } = data;
     return (
-        <li>
+        <li className="card">
             <div>
                 <img src={img_url} alt="name" width="40%" height="40%" />
             </div>
+            <div className="card-content">
             <h2>Name: {name} </h2>
             <h3>Rating: {rating} </h3>
             <p>Summary: {summary} </p>
@@ -13,6 +14,7 @@ export const SeriesCard = ({data}) => {
             <a href={watch_url} target="_blank">
                 <button>Watch Now</button>
             </a>
+        </div>
         </li>
     );
 }
