419d6e33d4d45ca6bfaded074666f9c68c70dbe9
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-05-24 23:36:07 +0500

    adding css in react.js

--- src/Components/Lists.jsx
+++ src/Components/Lists.jsx
@@ -1,29 +1,29 @@
 import './Netflix.css'
 
-export const SeriesCard = ({data}) => {
-    const {  img_url, name, rating, summary, genre, cast, watch_url } = data;
+export const SeriesCard = ({ data }) => {
+    const { img_url, name, rating, summary, genre, cast, watch_url } = data;
     const btn_style = {
-                    padding: "1.2rem 2.4rem",
-                    border: "none",
-                    fontSize: "1.6rem",
-                    backgroundColor: "rgb(49, 107, 201)",
-                    color: "white",
-                }
+        padding: "1.2rem 2.4rem",
+        border: "none",
+        fontSize: "1.6rem",
+        backgroundColor: "rgb(49, 107, 201)",
+        color: "white",
+    }
     return (
         <li className="card">
             <div>
                 <img src={img_url} alt="name" width="40%" height="40%" />
             </div>
             <div className="card-content">
-            <h2>Name: {name} </h2>
-            <h3>Rating: {rating} </h3>
-            <p>Summary: {summary} </p>
-            <p>Genre: {genre} </p>
-            <p>Cast: {cast} </p>
-            <a href={watch_url} target="_blank">
-                <button style={btn_style}>Watch Now</button>
-            </a>
-        </div>
+                <h2>Name: {name} </h2>
+                <h3>Rating: {rating} </h3>
+                <p>Summary: {summary} </p>
+                <p>Genre: {genre} </p>
+                <p>Cast: {cast} </p>
+                <a href={watch_url} target="_blank">
+                    <button style={btn_style}>Watch Now</button>
+                </a>
+            </div>
         </li>
     );
 }
