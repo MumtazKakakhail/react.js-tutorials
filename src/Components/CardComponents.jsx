1f8be1874ebb3899544529be402b38053ac11477
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-05-24 23:43:11 +0500

    adding css in react.js

--- src/Components/CardComponents.jsx
+++ src/Components/CardComponents.jsx
@@ -6,7 +6,7 @@ export const App = () => {
     <section className="container">
       <h1 className="card-heading">List of Best Netflix Series</h1>
       <NetflixSeries />
-    
+
     </section>
   );
 };
