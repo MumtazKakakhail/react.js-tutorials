8ca0f970b4eea30dde46ba151a30688d3058d94e
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-05-29 18:57:30 +0500

    CSS Modules in React.js

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import React from 'react'
 // import Practices from './Practices'
 import NetflixSeries from './Components/NetflixSeries';
 import Profile from './Components/Profile';
-import './Components/Netflix.css'
+// import './Components/Netflix.css';
+import './Components/Netflix.module.css';
 export const App = () => {
   return (
     <section className="container">
