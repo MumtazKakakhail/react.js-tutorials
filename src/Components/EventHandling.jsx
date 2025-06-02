79a7483c2d336c1780d45777e6bdf959b01e8b1f
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-06-02 20:39:40 +0500

    using EventHandling in react.js

--- src/Components/EventHandling.jsx
+++ src/Components/EventHandling.jsx
@@ -6,41 +6,41 @@ export const EventHandling = () => {
     // }
 
     const handleButtonClick = (event) => {
-           console.log(event);
-           console.log(event.target);
-           console.log(event.type);
-        
-        
-        
-         alert("Hey I am onClick event");
+        console.log(event);
+        console.log(event.target);
+        console.log(event.type);
+
+
+
+        alert("Hey I am onClick event");
     }
 
     const handleWelcomeUser = (user) => {
         console.log(`hey ${user}, welcome`);
-        
+
     }
     return (
-    <>
-    {/* function component with named function */}
-    <button onClick={handleButtonClick}>Click Me</button>
-    <br />
-    <br />
-    <br />
-    {/* flat arrow function */}
-      <button onClick={() => handleButtonClick()}>Click Me 2</button>
+        <>
+            {/* function component with named function */}
+            <button onClick={handleButtonClick}>Click Me</button>
+            <br />
+            <br />
+            <br />
+            {/* flat arrow function */}
+            <button onClick={() => handleButtonClick()}>Click Me 2</button>
 
-      {/* inline event handlers */}
-      {/* <button onClick={(event) => console.log(event);
+            {/* inline event handlers */}
+            {/* <button onClick={(event) => console.log(event);
       }>Inline Function</button> */}
-<br />
-<br />
-<br />
-      {/* function components with Inline arrow functions */}
-      <button onClick={() => alert("Hey i am inline event function")}>Inline arr fun</button>
+            <br />
+            <br />
+            <br />
+            {/* function components with Inline arrow functions */}
+            <button onClick={() => alert("Hey i am inline event function")}>Inline arr fun</button>
 
-      <br /><br /><br />
-      {/* passing arguments to event handler  */}
-      <button onClick={() => handleWelcomeUser("mumtaz")}>Click Me</button>
-    </>
+            <br /><br /><br />
+            {/* passing arguments to event handler  */}
+            <button onClick={() => handleWelcomeUser("mumtaz")}>Click Me</button>
+        </>
     );
 };
