c1c88961b2575acff43dac46d2abd90751c5b291
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-06-02 20:37:54 +0500

    using EventHandling in react.js

--- src/Components/EventHandling.jsx
+++ src/Components/EventHandling.jsx
@@ -1,3 +1,4 @@
+import "./Ev.css";
 export const EventHandling = () => {
 
     // function handleButtonClick() {
@@ -13,6 +14,11 @@ export const EventHandling = () => {
         
          alert("Hey I am onClick event");
     }
+
+    const handleWelcomeUser = (user) => {
+        console.log(`hey ${user}, welcome`);
+        
+    }
     return (
     <>
     {/* function component with named function */}
@@ -22,6 +28,19 @@ export const EventHandling = () => {
     <br />
     {/* flat arrow function */}
       <button onClick={() => handleButtonClick()}>Click Me 2</button>
+
+      {/* inline event handlers */}
+      {/* <button onClick={(event) => console.log(event);
+      }>Inline Function</button> */}
+<br />
+<br />
+<br />
+      {/* function components with Inline arrow functions */}
+      <button onClick={() => alert("Hey i am inline event function")}>Inline arr fun</button>
+
+      <br /><br /><br />
+      {/* passing arguments to event handler  */}
+      <button onClick={() => handleWelcomeUser("mumtaz")}>Click Me</button>
     </>
     );
 };
