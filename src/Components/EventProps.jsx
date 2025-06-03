9120b35862dff9f4d7cbbba1edbb4aca71fe373c
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-06-03 19:51:03 +0500

    Passing Events as Props in React | Control Parent Function Using Child Component

--- src/Components/EventProps.jsx
+++ src/Components/EventProps.jsx
@@ -1,7 +1,16 @@
 export const EventProps = () => {
+
+    const HandleWelcomeUser = (user) => {
+alert(`hey , ${user}`);
+    }
+
+    const handleHover = () => {
+        alert(`hey, thanks for hovering me!`)
+    }
     return (
 <>
-
+<WelcomeUser onClick = {() => HandleWelcomeUser("wali")} 
+    onMouseEnter={handleHover}/>
 
 </>
 
