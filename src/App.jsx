6f4632e711deae7c5aabfa2418008bb24a2a8fb2
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-06-10 20:51:55 +0500

    Event Propagation in React: Capturing, Bubbling, and Practical Implementation

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,17 @@ import NetflixSeries from './Components/NetflixSeries';
 // import Profile from './Components/Profile';
 // import './Components/Netflix.css';
 import "./Components/Netflix.module.css";
+import { EventPropagation } from './Components/EventPropagation';
 // import { EventHandling } from './Components/EventHandling';
-import { EventProps } from './Components/EventProps';
+// import { EventProps } from './Components/EventProps';
 export const App = () => {
   return (
     <section className="container">
       {/* <h1 className="card-heading">List of Best Netflix Series</h1> */}
       {/* <NetflixSeries /> */}
       {/* <EventHandling/> */}
-      <EventProps />
-
+      {/* <EventProps /> */}
+<EventPropagation/>
     </section>
   );
 };
