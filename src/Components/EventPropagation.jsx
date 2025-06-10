cdf861529ef8be26c7ca9150bf9d12f26a0a5a00
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-06-10 19:53:35 +0500

    Event Propagation in React: Capturing, Bubbling, and Practical Implementation

--- src/Components/EventPropagation.jsx
+++ src/Components/EventPropagation.jsx
@@ -1,14 +1,27 @@
-const EventPropagation = () => {
-return (
-<section className="main-div">
-    <div className="g-div" onClick={handleGrandParent}>
-<div className="p-div" onClick={handleParentClick}>
-<button className="c-div" onClick={handleChildClick}>Child Div</button>
-</div>
-    </div>
+import "./EV.css";
+export const EventPropagation = () => {
+  const handleGrandParent = () => {
+    console.log("GrandParent clicked");
+  };
 
-</section>
+  const handleParentClick = () => {
+    console.log("Parent clicked");
+  };
 
-
-)
-}
+  const handleChildClick = (event) => {
+    console.log(event);
+    event.stopPropagation();
+    console.log("Child clicked");
+  };
+  return (
+    <section className="main-div">
+      <div className="g-div" onClickCapture={handleGrandParent}>
+        <div className="p-div" onClickCapture={handleParentClick}>
+          <button className="c-div" onClickCapture={handleChildClick}>
+            Child Div
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
