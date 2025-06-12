caf65d51074b624e80cb15b2f5fdfad423b09217
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-06-12 21:59:01 +0500

    How React State Works

--- src/Components/hooks/State.jsx
+++ src/Components/hooks/State.jsx
@@ -5,31 +5,31 @@ export const State = () => {
     // const handleButtonClick = () => {
     //     value ++;
     //     console.log(value);
-        
+
     // }
     // let array = useState();
     // console.log(array);
     const [count, setCount] = useState(0);
     console.log("parent component rendered");
-    
+
     const handleButtonClick = () => {
-        setCount (() => count +1);
+        setCount(() => count + 1);
     };
     return (
         <>
-        <div className="main-div">
-        <h1>{count}</h1>
-        <button onClick={handleButtonClick}>Increment</button>
-        </div>
-        <ChildComponent count={count}/>
+            <div className="main-div">
+                <h1>{count}</h1>
+                <button onClick={handleButtonClick}>Increment</button>
+            </div>
+            <ChildComponent count={count} />
         </>
     );
 };
 
-function ChildComponent({count}) {
+function ChildComponent({ count }) {
     console.log("Child Component rendered");
     return (
-    <div className="main-div">
-        <h2>Child Component  -{count} </h2></div>
+        <div className="main-div">
+            <h2>Child Component  -{count} </h2></div>
     );
 };
