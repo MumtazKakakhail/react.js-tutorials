6b05673389fc3a36bdefcf708256cba2a1ca92dd
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-06-12 20:03:04 +0500

    How React State Works

--- src/Components/hooks/State.jsx
+++ src/Components/hooks/State.jsx
@@ -1,15 +1,22 @@
+import { useState } from "react";
 import "../Ev.css";
 export const State = () => {
-    let value = 0;
-    const handleButtonClick = () => {
-        value ++;
-        console.log(value);
+    // let value = 0;
+    // const handleButtonClick = () => {
+    //     value ++;
+    //     console.log(value);
         
-    }
+    // }
+    // let array = useState();
+    // console.log(array);
+    const [count, setCount] = useState(0);
+    const handleButtonClick = () => {
+        setCount (() => count +1);
+    };
     return (
         <>
         <section className="main-div">
-        <h1>{value}</h1>
+        <h1>{count}</h1>
         <button onClick={handleButtonClick}>Increment</button>
         </section>
         </>
