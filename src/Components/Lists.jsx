852dfcc6ed307dd630828feaa3c14c3d8e7e2135
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-06-16 21:51:50 +0500

    hooks in react.js

--- src/Components/Lists.jsx
+++ src/Components/Lists.jsx
@@ -7,27 +7,27 @@ export const SeriesCard = ({ data }) => {
 
 
   // const btn_style = {}
-//   const ButtonOne = styled.button ({
-//  padding: "1.2rem 2.4rem",
-//     border: "none",
-//     fontSize: "1.6rem",
-//     backgroundColor: `${rating >= 97 ? " rgb(37, 219, 219)" : " rgb(240, 191, 132)"}`,
-//     color: "var(--btn-color)",
-//     fontWeight: "bold",
-//     cursor: "pointer",
-//   });
-   
+  //   const ButtonOne = styled.button ({
+  //  padding: "1.2rem 2.4rem",
+  //     border: "none",
+  //     fontSize: "1.6rem",
+  //     backgroundColor: `${rating >= 97 ? " rgb(37, 219, 219)" : " rgb(240, 191, 132)"}`,
+  //     color: "var(--btn-color)",
+  //     fontWeight: "bold",
+  //     cursor: "pointer",
+  //   });
+
   const ButtonOne = styled.button`
  padding: 1.2rem 2.4rem;
     border: none;
     font-Size: 1.6rem;
-    background-Color: ${(props) => props.rating >= 97 ? " rgb(37, 219, 219)" :  "rgb(240, 191, 132)"};
+    background-Color: ${(props) => props.rating >= 97 ? " rgb(37, 219, 219)" : "rgb(240, 191, 132)"};
     color: var(--btn-color);
     font-Weight: bold;
     cursor: pointer;
   `;
-   
-  
+
+
   const ratingClass = rating >= 97 ? styles.super : styles.average;
   return (
     <li className={styles.card}>
@@ -47,8 +47,8 @@ export const SeriesCard = ({ data }) => {
         <p>Cast: {cast} </p>
         <a href={watch_url} target="_blank">
           {/* <button style={btn_style}>Watch Now</button> */}
-        {/* <ButtonOne>Watch Now</ButtonOne> */}
-         <ButtonOne rating={rating}>Watch Now</ButtonOne>
+          {/* <ButtonOne>Watch Now</ButtonOne> */}
+          <ButtonOne rating={rating}>Watch Now</ButtonOne>
         </a>
       </div>
     </li>
