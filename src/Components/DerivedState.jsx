dee61cbe3d50b4a63527d240f44c66243510fd67
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-06-17 19:16:49 +0500

    React JS State Challenge

--- src/Components/DerivedState.jsx
+++ src/Components/DerivedState.jsx
@@ -1,28 +1,30 @@
+import { useState } from "react";
 import "../Components/Ev.css"
-const users = [
-    { "name": "Alice", age: 25 },
-    { "name": "Lewis", age: 30 },
-    { "name": "Charlie", age: 22 },
-    { "name": "Angle", age: 28 },
-    { "name": "Thomas", age: 18 }
-];
+// const users = [
+//     { "name": "Alice", age: 25 },
+//     { "name": "Lewis", age: 30 },
+//     { "name": "Charlie", age: 22 },
+//     { "name": "Angle", age: 28 },
+//     { "name": "Thomas", age: 18 }
+// ];
 
 export const DerivedState = () => {
+     const [users] = useState([
+        { name: "Alice", age: 25 },
+        { name: "Lewis", age: 30 },
+        { name: "Charlie", age: 22 },
+        { name: "Angle", age: 28 },
+        { name: "Thomas", age: 18 }
+    ]);
     return (
         <div className="main-div">
             <h1>User List</h1>
             <ul>
-                {
-                    users.map((curElem, index) => {
-                        return (
-                        
-                            <li key={index}>
-                                {curElem.name} - {curElem.age} year old
-                            </li>
-                            
-                        );
-                    })
-                }
+                {users.map((curElem, index) => (
+                    <li key={curElem.name + index}>
+                        {curElem.name} - {curElem.age} years old
+                    </li>
+                ))}
             </ul>
         </div>
     );
