423434271702bee0da01e24fe904a1485ec0230d
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-06-18 19:04:34 +0500

    Toggle Switch using react.js

---
+++ src/projects/ToggleSwitch/ToggleSwitch.jsx
@@ -0,0 +1,9 @@
+export const ToggleSwitch = () => {
+    return (
+        <div className="toggle-switch">
+            <div className="switch">
+                <span className="switch-state">ON</span>
+            </div>
+        </div>
+    );
+};
