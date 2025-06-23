b9ca85ce70190828e9b0e89549b5d7c214a3d533
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-06-23 19:03:48 +0500

    Create a Real-Time Digital Clock in React

--- src/projects/Todo/Todo.jsx
+++ src/projects/Todo/Todo.jsx
@@ -1,78 +1,83 @@
 import { useState } from "react";
 import "./Todo.css";
-import { MdCheck, MdDeleteForever } from "react-icons/md";
+
+
 export const Todo = () => {
-    const [inputValue, setInputValue] = useState("");
-    const [task, setTask] = useState([]);
-    const [dateTime, setDateTime] = useState("");
+  const [task, setTask] = useState(() => {
 
+  });
 
-    const handleInputChange = (value) => {
-        setInputValue(value);
-    };
+  const handleFormSubmit = (inputValue) => {
+    const { id, content, checked } = inputValue;
 
-    const handleFormSubmit = (event) => {
-        event.preventDefault();
-    };
+    //to check if the input field is empty or not
+    if (!content) return;
 
-    if(!inputValue) return;
-if (task.includes(inputValue)){
-     setInputValue("");
-    return;
-} ;
-    setTask((prevTask) => [...prevTask, inputValue ]
-     );
-
-     setInputValue("");
-
-    const getDateTime = () => {
-  const now = new Date();
-     const formattedDate = now.toLocaleDateString();
-     const formattedTime = now.toLocaleTimeString();
-     setDateTime(`${formattedDate} - ${formattedTime}`);
-    }
-
-     setInterval(() => {
-        getDateTime();
-     }, 1000);
-    return (
-        <section className="todo-container">
-<header>
-    <h1>Todo List</h1>
-    <h2 className="date-time">{dateTime}</h2>
-</header>
-<section className="form">
-    <form onSubmit={handleFormSubmit}>
-        <div>
-            <input type="text" className="todo-input"
-             autoComplete="off" value={inputValue}
-            onChange={(event) => handleInputChange(event.target.value)} />
-        </div>
-        <div>
-            <button type="submit" className="todo-btn">
-                Add Task
-            </button>
-        </div>
-    </form>
-</section>
-<section className="myUnOrdList">
-    <ul>
-        {
-            task.map((curTask , index) => {
-             return <li key={index} className="todo-item"> 
-<span>{curTask}</span>
-<button className="check-btn">
-<MdCheck />
-</button>
-<button className="delete-btn">
-<MdDeleteForever/>
-</button>
-             </li>
-            })
-        };
-    </ul>
-</section>
-        </section>
+    // to check if the data is already existing or not
+    // if (task.includes(inputValue)) return;
+    const ifTodoContentMatched = task.find(
+      (curTask) => curTask.content === content
     );
+    if (ifTodoContentMatched) return;
 
+    setTask((prevTask) => [...prevTask, { id, content, checked }]);
+  };
+
+  //todo add data to localStorage
+
+
+  //todo handleDeleteTodo function
+  const handleDeleteTodo = (value) => {
+    const updatedTask = task.filter((curTask) => curTask.content !== value);
+    setTask(updatedTask);
+  };
+
+  //todo handleClearTodoData functionality
+  const handleClearTodoData = () => {
+    setTask([]);
+  };
+
+  //todo handleCheckedTodo functionality
+  const handleCheckedTodo = (content) => {
+    const updatedTask = task.map((curTask) => {
+      if (curTask.content === content) {
+        return { ...curTask, checked: !curTask.checked };
+      } else {
+        return curTask;
+      }
+    });
+    setTask(updatedTask);
+  };
+
+  return (
+    <section className="todo-container">
+      <header>
+        <h1>Todo List</h1>
+        <TodoDate />
+      </header>
+
+      <TodoForm onAddTodo={handleFormSubmit} />
+
+      <section className="myUnOrdList">
+        <ul>
+          {task.map((curTask) => {
+            return (
+              <TodoList
+                key={curTask.id}
+                data={curTask.content}
+                checked={curTask.checked}
+                onHandleDeleteTodo={handleDeleteTodo}
+                onHandleCheckedTodo={handleCheckedTodo}
+              />
+            );
+          })}
+        </ul>
+      </section>
+      <section>
+        <button className="clear-btn" onClick={handleClearTodoData}>
+          Clear all
+        </button>
+      </section>
+    </section>
+  );
 };
